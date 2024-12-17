9f81224c1b4a511ee746bfbe6a5d4774e1b34c21
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2024-12-17 12:08:05 +0100

    author created EnterXorO() method with parameters player and input, that will assign 'X' or 'O' mark on field. and also made executive code shorten

--- TicTacToeChallenge/AuthorSolution.cs
+++ TicTacToeChallenge/AuthorSolution.cs
@@ -19,45 +19,18 @@ SetField();
 
 do
 {
-    if (player == 2) player = 1;
-    else if (player == 1) player = 2;
-
-    switch (player)
+    if (player == 2)
     {
-        case 1: // Player 1's ture
-            switch (input)
-            {
-                case 1: playField[0, 0] = 'X'; break;
-                case 2: playField[0, 1] = 'X'; break;
-                case 3: playField[0, 2] = 'X'; break;
-
-                case 4: playField[1, 0] = 'X'; break;
-                case 5: playField[1, 1] = 'X'; break;
-                case 6: playField[1, 2] = 'X'; break;
-
-                case 7: playField[2, 0] = 'X'; break;
-                case 8: playField[2, 1] = 'X'; break;
-                case 9: playField[2, 2] = 'X'; break;
-            }
-            break;
-
-        case 2: // Player 2's ture
-            switch (input)
-            {
-                case 1: playField[0, 0] = 'O'; break;
-                case 2: playField[0, 1] = 'O'; break;
-                case 3: playField[0, 2] = 'O'; break;
-
-                case 4: playField[1, 0] = 'O'; break;
-                case 5: playField[1, 1] = 'O'; break;
-                case 6: playField[1, 2] = 'O'; break;
-
-                case 7: playField[2, 0] = 'O'; break;
-                case 8: playField[2, 1] = 'O'; break;
-                case 9: playField[2, 2] = 'O'; break;
-            }
-            break;
+        player = 1;
+        EnterXorO(player, input);
     }
+    else if (player == 1)
+    {
+        player = 2;
+        EnterXorO(player, input);
+    }
+
+    
     
     
     
@@ -78,4 +51,29 @@ void SetField()
     Console.WriteLine("     |     |     ");
     Console.WriteLine("  {0}  |  {1}  |  {2}  ", playField[2, 0], playField[2, 1], playField[2, 2]);
     Console.WriteLine("     |     |     ");
+}
+
+// public static void EnterXorO(int player, int input)
+void EnterXorO(int player, int input)
+{
+    char playerSign = ' ';
+    if (player == 1)
+        playerSign = 'X';
+    else if (player == 2)
+        playerSign = 'O';
+
+    switch (input)
+    {
+        case 1: playField[0, 0] = playerSign; break;
+        case 2: playField[0, 1] = playerSign; break;
+        case 3: playField[0, 2] = playerSign; break;
+
+        case 4: playField[1, 0] = playerSign; break;
+        case 5: playField[1, 1] = playerSign; break;
+        case 6: playField[1, 2] = playerSign; break;
+
+        case 7: playField[2, 0] = playerSign; break;
+        case 8: playField[2, 1] = playerSign; break;
+        case 9: playField[2, 2] = playerSign; break;
+    }
 }
